f1ee67313e3d7f7200212b3744945a32c326dd76
le-e-e <lbc231@naver.com>
2023-04-26 14:31:19 +0000

    2023/04/26

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,11 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("hi %s." % question_id)
+
+def results(request, question_id):
+    response = "result of question %s."
+    return HttpResponse(response % question_id)
+
